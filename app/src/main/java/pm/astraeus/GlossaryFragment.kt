c569be9a770accc62f55c821a2cfccf76989c31b
DanielPinho1143 <dsp.danielpinho@gmail.com>
2024-12-06 01:44:06 +0000

    adicionado tabs

--- app/src/main/java/pm/astraeus/GlossaryFragment.kt
+++ app/src/main/java/pm/astraeus/GlossaryFragment.kt
@@ -5,29 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GlossaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GlossaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +17,35 @@ class GlossaryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_glossary, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GlossaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GlossaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val root: ViewGroup = view.findViewById(R.id.glossaryInclude)
+        val inflater = LayoutInflater.from(requireContext())
+        val tabs: TabLayout = view.findViewById(R.id.tabGlossary)
+
+        tabs.addTab(tabs.newTab().setText("Tab 1"))
+        tabs.addTab(tabs.newTab().setText("Tab 2"))
+        tabs.addTab(tabs.newTab().setText("Tab 3"))
+        tabs.tabGravity = TabLayout.GRAVITY_FILL
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                root.removeAllViews()
+                when (tabs.selectedTabPosition) {
+                    0 -> inflater.inflate(R.layout.tab_tipo_missao, root, true)
+                    1 -> inflater.inflate(R.layout.tab_tipo_corpo, root, true)
+                    2 -> inflater.inflate(R.layout.tab_estado_missao, root, true)
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Optionally handle tab unselected
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Optionally handle tab reselected
+            }
+        })
     }
 }
